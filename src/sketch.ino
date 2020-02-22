f0a7c583a432b9ca8168f4a76168bb7ff4a2e5e6
Jason Dunsmore <jasondunsmore@gmail.com>
2020-02-21 19:10:17 -0600

    Added break, etc..

--- src/sketch.ino
+++ src/sketch.ino
@@ -4,8 +4,12 @@ const int led3 = 6;
 const int led4 = 9; 
 const int led5 = 10;
 const int button = 2;
-const int timerMin = 25;  // How long should the timer run for
-const int timerSec = (timerMin * 60) / 5;
+//const int timerMin = 25;
+//const int shortBreakMin = 5;
+//const int longBreakMin = 10;
+const int timerMin = 1;
+const int shortBreakMin = 1;
+const int longBreakMin = 2;
 
 int buttonState = 0;
 int fadeAmount = 5;
@@ -17,9 +21,9 @@ void setup() {
   Serial.begin(9600);
 }
 
-void breathe(int led) {
+void breathe(int led, int secs) {
   long msec = 0;
-  while (msec < timerSec*1000L) {
+  while (msec < secs*1000L) {
     analogWrite(led, brightness);
     brightness = brightness + fadeAmount;
 
@@ -50,46 +54,109 @@ void allOff() {
   analogWrite(led5, 0);
 }
 
-void done() {
-  nDone = nDone + 1;
-  for (int i = 0; i <= 5; i++) {
-    allOn();
-    delay(200);
-    allOff();
-    delay(200);
+void takeBreak(int bsecs) {
+  allOff();
+  int secs = bsecs / 5;
+  breathe(led5, secs);
+  breathe(led4, secs);
+  breathe(led3, secs);
+  breathe(led2, secs);
+  breathe(led1, secs);
+  allOff();
+}
+
+void waitBreak() {
+  // slow blink until button press
+  long interval = 2000;
+  long previousMillis = 0;
+  int ledState = LOW;
+  while (digitalRead(button) == LOW) {
+    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis > interval) {
+      previousMillis = currentMillis;
+      if (ledState == LOW) {
+        allOn();
+        ledState = HIGH;
+      } else {
+        allOff();
+        ledState = LOW;
+      }
+    }
   }
+}
+
+void breakDone() {
+  nDone = nDone + 1;
+  waitBreak();
+
+  // long break every 4 work sessions
+  if (nDone % 4 == 0) {
+    takeBreak(shortBreakMin * 60);
+  } else {
+    takeBreak(longBreakMin * 60);
+  }
+
+  // indicator for how many have been completed
   switch (nDone) {
   case 1:
-    analogWrite(led1, 0);
+    analogWrite(led1, 255);
+    break;
   case 2:
-    analogWrite(led1, 0);
-    analogWrite(led2, 0);
+    analogWrite(led1, 255);
+    analogWrite(led2, 255);
+    break;
   case 3:
-    analogWrite(led1, 0);
-    analogWrite(led2, 0);
-    analogWrite(led3, 0);
+    analogWrite(led1, 255);
+    analogWrite(led2, 255);
+    analogWrite(led3, 255);
+    break;
   case 4:
-    analogWrite(led1, 0);
-    analogWrite(led2, 0);
-    analogWrite(led3, 0);
-    analogWrite(led4, 0);
+    analogWrite(led1, 255);
+    analogWrite(led2, 255);
+    analogWrite(led3, 255);
+    analogWrite(led4, 255);
+    break;
   case 5:
-    analogWrite(led1, 0);
-    analogWrite(led2, 0);
-    analogWrite(led3, 0);
-    analogWrite(led4, 0);
-    analogWrite(led5, 0);
+    analogWrite(led1, 255);
+    analogWrite(led2, 255);
+    analogWrite(led3, 255);
+    analogWrite(led4, 255);
+    analogWrite(led5, 255);
+    break;
+  case 6:
+    analogWrite(led1, 255);
+    analogWrite(led5, 255);
+    break;
+  case 7:
+    analogWrite(led2, 255);
+    break;
+  case 8:
+    analogWrite(led3, 255);
+    break;
+  case 9:
+    analogWrite(led4, 255);
+    break;
+  case 10:
+    analogWrite(led5, 255);
+    break;
   }
+}
+
+void work() {
+  allOff();
+  int sec = (timerMin * 60) / 5;
+  breathe(led1, sec);
+  breathe(led2, sec);
+  breathe(led3, sec);
+  breathe(led4, sec);
+  breathe(led5, sec);
+  allOff();
 }
 
 void loop() {
   buttonState = digitalRead(button);
   if (buttonState == HIGH) {
-    breathe(led1);
-    breathe(led2);
-    breathe(led3);
-    breathe(led4);
-    breathe(led5);
-    done();
+    work();
+    breakDone();
   }
 }
